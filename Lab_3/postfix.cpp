a745edad5066fc6cb6080ef03692803ffc91d578
8585712 <8585712@COIS.local>
2019-09-24 10:23:11 -0500

    Add project files.

---
+++ Lab_3/postfix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	string postfixExp = "";
+	string infixExp;
+	for (char& ch : infixExp) {
+		switch (ch) {
+			case isdigit(ch) {
+
+			}
+		}
+	}
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+/*
+1. Read a string of a mathematical expression
+2. Extract operands and operators and push them into a stack
+3. Calculate the values starting from the top of the stack
+
+
+
+
+
+
+
+*/
